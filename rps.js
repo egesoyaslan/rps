c023cba150c2e8b6744f6494316871fe28372103
egesoyaslan <ege.soyaslan@yahoo.com>
2023-12-15 19:12:03 +0300

    Edit rps.js
    Added the function playRound that simulates one round of RPS.

--- rps.js
+++ rps.js
@@ -1,22 +1,41 @@
-function getComputerChoice() {
+function getComputerChoice(result) {
     let num = Math.floor(Math.random() * 3) +1;
-    let result;
     switch(num) {
         case 1:
-            result = `Rock`
+            result = `rock`
             break;
         case 2:
-            result = `Paper`
+            result = `paper`
             break;
         case 3:
-            result = `Scissors`
+            result = `scissors`
             break;
     }
     return result;
 }
 
-function playerSelection() {
-    let userInput = prompt("Rock, Paper or Scissors?");
+function getPlayerChoice(userInput) {
+    userInput = prompt("Rock, Paper or Scissors?");
     return userInput.toLowerCase();
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+
+    if (playerSelection === `rock` && computerSelection === `scissors`) {
+        alert(`You win! Rock beats Scissors`);
+    } else if (playerSelection === `scissors` && computerSelection === `paper`) {
+        alert(`You win! Scissors beats Paper`);
+    } else if (playerSelection === `paper` && computerSelection === `rock`) {
+        alert(`You win! Paper beats Rock`);
+    } else if (playerSelection === `rock` && computerSelection === `paper`) {
+        alert(`You loose! Paper beats Rock`);
+    } else if (playerSelection === `paper` && computerSelection === `scissors`) {
+        alert(`You loose! Scissors beats Paper`);
+    } else if (playerSelection === `scissors` && computerSelection === `rock`) {
+        alert(`You loose! Rock beats Scissors`);
+    } else {
+        alert(`Its a Tie! Play again`);
+        playRound(playerSelection, computerSelection)}
+}
